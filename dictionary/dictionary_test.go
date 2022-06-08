6e256bc8c05f0b5a0ab6ad1ebed13024f4b9994d
Ry <rybulloch@gmail.com>
2022-06-08 08:18:42 +0100

    Update dictionary_test.go

--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -25,9 +25,9 @@ func TestSearch(t *testing.T) {
 func TestAdd(t *testing.T) {
 
 	t.Run("new word", func(t *testing.T) {
-		dictionary := Dictionary{}
 		word := "test"
 		definition := "this is just a test"
+		dictionary := Dictionary{}
 
 		err := dictionary.Add(word, definition)
 
@@ -49,14 +49,45 @@ func TestAdd(t *testing.T) {
 }
 
 func TestUpdate(t *testing.T) {
-	word := "test"
-	definition := "this is just a test"
-	dictionary := Dictionary{word: definition}
-	newDefinition := "new definition"
 
-	dictionary.Update(word, newDefinition)
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		newDefinition := "new definition"
+		dictionary := Dictionary{word: definition}
 
-	assertDefinition(t, dictionary, word, newDefinition)
+		err := dictionary.Update(word, newDefinition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
+
+	t.Run("new word", func(t *testing.T) {
+		//key
+		word := "test"
+		//value
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, ErrWordDoesNotExist)
+	})
+}
+
+func TestDelete(t *testing.T) {
+	t.Run("delete word", func(t *testing.T) {
+		word := "test"
+		dictionary := Dictionary{word: "test definition"}
+
+		dictionary.Delete(word)
+
+		_, err := dictionary.Search(word)
+		if err != ErrNotFound {
+			t.Errorf("Expected %q to be deleted", word)
+		}
+	})
+
 }
 
 func assertStrings(t testing.TB, got, want string) {
